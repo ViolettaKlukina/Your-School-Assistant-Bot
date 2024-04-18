dbcc15400790c65206998fe8b26e69bdd22f7e52
Виолетта <violeklyukina@yandex.ru>
2024-04-18 23:53:27 +0300

    Add file

--- config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = "7061472990:AAGMjsNqzMMnR8PuZ8koxvSnNVZ7lHzUgRM"
-SERVER = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-MODEL = "yandexgpt-lite"
-administrators = []
-
